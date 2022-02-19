a63ad4d6419dd84eff9eed2b17a48537f9202a92
Agya <61568773+adeshrai6@users.noreply.github.com>
2022-02-18 23:58:04 -0500

    Table

--- JavaFx/Fx/src/main/java/com/example/javafx/JavaFxController.java
+++ JavaFx/Fx/src/main/java/com/example/javafx/JavaFxController.java
@@ -1,11 +1,21 @@
 package com.example.javafx;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
 public class JavaFxController {
+
+    @FXML
+    private TableColumn<?, ?> age;
+
+    @FXML
+    private TableColumn<?, ?> animals;
+
+    @FXML
+    private TableColumn<?, ?> name;
 
     @FXML
     private TextField nameIn;
