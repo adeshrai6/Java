265428df0d54ffe92f7b931dc6cd9a0900a5190f
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-03-04 22:44:22 -0500

    getting number from user and add to list

--- Lottrey/src/Lottrey.java
+++ Lottrey/src/Lottrey.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Lottrey {
     public static void main(String[] args) {
@@ -19,6 +20,23 @@ public class Lottrey {
 
         }
         System.out.println(winningNumbers);
+
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> pickedNumber = new ArrayList<>();
+        System.out.print("Please enter your number:");
+
+        for (int i = 0; i < 6; i++) {
+            while (true) {
+                String stringNumber = scanner.nextLine();
+                int pick = Integer.parseInt(stringNumber);
+
+                if (pick > 0 && pick < 50) {
+                    pickedNumber.add(pick);
+                    break;
+                }
+            }
+        }
+
 
     }
 }
