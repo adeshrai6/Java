9e371f08f541923f2807a45c0a9c354886f20c14
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-03-05 23:44:53 -0500

    player turn using switch board

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -1,15 +1,43 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean isValidMove = false;
         char[][] board = {{' ', ' ', ' '},
                 {' ', ' ', ' '},
                 {' ', ' ', ' '}};
         printBoard(board);
         playerTurn(board, scanner);
+
+        Random random = new Random();
+
+        while (true) {
+            int computerPlay = random.nextInt(9) + 1;
+            if(isValidMove){
+
+            }
+        }
+
     }
 
+    private static boolean isSpaceAvailable(char[][] board, int position){
+        switch (position){
+            case 1: return (board[0][0] == ' ');
+            case 2: return (board[0][1] == ' ');
+            case 3: return (board[0][2] == ' ');
+            case 4: return (board[1][0] == ' ');
+            case 5: return (board[1][1] == ' ');
+            case 6: return (board[1][2] == ' ');
+            case 7: return (board[2][0] == ' ');
+            case 8: return (board[2][1] == ' ');
+            case 9: return (board[2][2] == ' ');
+            default:
+                System.out.println("Not valid move");
+                return false;
+        }
+    }
     private static void playerTurn(char[][] board, Scanner scanner) {
 
         scanner = new Scanner(System.in);
