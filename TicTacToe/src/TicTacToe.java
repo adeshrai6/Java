568673af65dee3e0bd58e99473d0e9cf885fc675
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-03-05 23:19:56 -0500

    player turn using switch board

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -1,16 +1,63 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
-        char[][] board  =  {{'1','2','3'},
-                            {'4','5','6'},
-                            {'7','8','9'}};
+        Scanner scanner = new Scanner(System.in);
+        char[][] board = {{' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}};
+        printBoard(board);
+        playerTurn(board, scanner);
+    }
 
+    private static void playerTurn(char[][] board, Scanner scanner) {
+
+        scanner = new Scanner(System.in);
+        System.out.print("Please enter '1-9' in the board:");
+        String userInput = scanner.nextLine();
+
+        switch (userInput) {
+            case "1":
+                board[0][0] = 'x';
+                break;
+            case "2":
+                board[0][1] = 'x';
+                break;
+            case "3":
+                board[0][2] = 'x';
+                break;
+            case "4":
+                board[1][0] = 'x';
+                break;
+            case "5":
+                board[1][1] = 'x';
+                break;
+            case "6":
+                board[1][2] = 'x';
+                break;
+            case "7":
+                board[2][0] = 'x';
+                break;
+            case "8":
+                board[2][1] = 'x';
+                break;
+            case "9":
+                board[9][0] = 'x';
+                break;
+            default:
+                System.out.println("you input wrong thing");
+        }
+
+        printBoard(board);
+
+    }
+
+    private static void printBoard(char[][] board) {
         System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
         System.out.println("–+–+–");
         System.out.println(board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
         System.out.println("–+–+–");
         System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
-
-
 
     }
 }
