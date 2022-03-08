edecb38cb0d855cbf758a8b3cc86cfb08f071491
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-03-07 23:59:44 -0500

    rest controller

--- movie-catalog-service/src/main/java/io/adesh/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/adesh/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,13 +1,17 @@
 package io.adesh.moviecatalogservice.resources;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Collections;
 
 @RestController
+@RequestMapping("/catalog")
 public class MovieCatalogResource{
 
-    public List<CatalogItem> getCatalog(String userId){
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
         return Collections.singletonList(
             new CatalogItem("Trans", "Test", 4)
         );
