11a399b2fc99b41aa3cd76f49670b3c0be492112
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-03-08 22:32:45 -0500

    movie-service

---
+++ micro-services/movie-info-service/src/test/java/io/adesh/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.adesh.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
