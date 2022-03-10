fccb1adeb0138c22ede7e139d7d20b2b6dbbfb1a
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-03-09 23:07:24 -0500

    catalogItem

--- micro-services/ratings-data-service/src/main/java/io/adesh/ratingsdataservice/resources/RatingsDataResource.java
+++ micro-services/ratings-data-service/src/main/java/io/adesh/ratingsdataservice/resources/RatingsDataResource.java
@@ -1,4 +1,5 @@
 package io.adesh.ratingsdataservice.resources;
+package io.adesh.ratingsdataservice.models.Rating;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,6 @@ public class RatingsDataResource {
     
     @RequestMapping("/{ratingsdata}")
     public Rating getRating(@PathVariable("movieId") String movieId){
-        return new Rating(movieId, 4)
+        return new Rating(movieId, 4);
     }
 }
