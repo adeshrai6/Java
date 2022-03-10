1526cf6cc416e2482b02b744278973bd1a8a8626
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-03-09 23:11:14 -0500

    Rating

--- micro-services/ratings-data-service/src/main/java/io/adesh/ratingsdataservice/resources/RatingsDataResource.java
+++ micro-services/ratings-data-service/src/main/java/io/adesh/ratingsdataservice/resources/RatingsDataResource.java
@@ -1,9 +1,10 @@
 package io.adesh.ratingsdataservice.resources;
-package io.adesh.ratingsdataservice.models.Rating;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.adesh.ratingsdataservice.models.Rating;
 
 @RestController
 @RequestMapping("/ratingsdata")
