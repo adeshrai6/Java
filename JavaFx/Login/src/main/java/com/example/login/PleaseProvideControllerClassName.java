f69a84afc3fc39798e89b35a04020faa98e292cf
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-04-05 15:09:23 -0400

    HousePick

---
+++ JavaFx/Login/src/main/java/com/example/login/PleaseProvideControllerClassName.java
@@ -0,0 +1,58 @@
+package com.example.login;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private Label chosenHome;
+
+    @FXML
+    private TextField chosenOutput;
+
+    @FXML
+    private Button findBtn;
+
+    @FXML
+    private Label maxBeds;
+
+    @FXML
+    private TextField maxBedsInput;
+
+    @FXML
+    private Label maximumArea;
+
+    @FXML
+    private TextField maximumAreaInput;
+
+    @FXML
+    private TextField maximumInput;
+
+    @FXML
+    private Label maximumPrice;
+
+    @FXML
+    private Label minBeds;
+
+    @FXML
+    private TextField minBedsInput;
+
+    @FXML
+    private Label minimumArea;
+
+    @FXML
+    private TextField minimumAreaInput;
+
+    @FXML
+    private TextField minimumInput;
+
+    @FXML
+    private Label minimumPrice;
+
+    @FXML
+    private Button reset;
+
+}
