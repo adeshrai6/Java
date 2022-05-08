ea70d9cc076fc7c471e917a7374b7b4274419d89
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-05-08 19:36:11 -0400

    login view

--- JavaFx/Invoice/src/main/java/com/example/invoice/Invoice.java
+++ JavaFx/Invoice/src/main/java/com/example/invoice/Invoice.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class Invoice extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Invoice.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Invoice.class.getResource("Invoice-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 450, 400);
-        stage.setTitle("Hello!");
+        stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
     }
