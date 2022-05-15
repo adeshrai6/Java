5259e2c2612a4071ce1d4e4510b7b4df72bf8ab6
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-05-14 23:19:28 -0400

    Main class

---
+++ DataStructure/TheaterTicketSystem/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Theatre theatre = new Theatre("Olompia", 8, 12);
+        theatre.getSeats();
+
+//        if(theatre.reserveSeat())
+    }
+}
