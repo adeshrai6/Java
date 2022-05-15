87f850941763c2c364d724a673e4415fbdcd6813
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-05-14 23:19:13 -0400

    Theater reverse method

--- DataStructure/TheaterTicketSystem/src/Theatre.java
+++ DataStructure/TheaterTicketSystem/src/Theatre.java
@@ -11,8 +11,35 @@ public class Theatre {
         for (char row = 'A'; row <= lastRow; row++) {
             for (int seatNum = 1; seatNum <= seatsPerRow; seatNum++) {
                 Seat seat = new Seat(row + String.format("%02d", seatNum));
-                seat.add(seat);
+                seats.add(seat);
             }
+        }
+    }
+
+    public String getTheatreName() {
+        return theatreName;
+    }
+
+    public boolean reserveSeat(String seatNumber) {
+        Seat requestedSeat = null;
+        for(Seat seat : seats) {
+            if(seat.getSeatNumber().equals(seatNumber)) {
+                requestedSeat = seat;
+                break;
+            }
+        }
+
+        if (requestedSeat == null) {
+            System.out.println("There is no seat " + seatNumber);
+            return false;
+        }
+
+        return requestedSeat.reserve();
+    }
+
+    public void getSeats() {
+        for (Seat seat : seats) {
+            System.out.println(seat.getSeatNumber());
         }
     }
 
