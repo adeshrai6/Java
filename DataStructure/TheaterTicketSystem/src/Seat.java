c0c9e863009b109362b857606cf7289bb8b66af9
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-05-14 23:18:09 -0400

    Seat calss

--- DataStructure/TheaterTicketSystem/src/Seat.java
+++ DataStructure/TheaterTicketSystem/src/Seat.java
@@ -1,3 +1,39 @@
 public class Seat {
+    private final String seatNumber;
+    private boolean reserve = false;
 
+    public Seat(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public boolean reserve(){
+        if(!this.reserve){
+            this.reserve = true;
+            System.out.println("Seat " + seatNumber + " reserved");
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean cancel() {
+        if(this.reserve) {
+            this.reserve = false;
+            System.out.println("Reservation of seat " + seatNumber + " cancelled");
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+    public boolean isReserve() {
+        return reserve;
+    }
+
+    public void setReserve(boolean reserve) {
+        this.reserve = reserve;
+    }
 }
