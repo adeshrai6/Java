76f25fc412039afbb7cc17545d2ce4a564d66427
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-05-16 23:46:01 -0400

    Printing Seat list

--- DataStructure/TheaterTicketSystem/src/Main.java
+++ DataStructure/TheaterTicketSystem/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Theatre theatre = new Theatre("Olompia", 8, 12);
@@ -5,4 +7,14 @@ public class Main {
 
 //        if(theatre.reserveSeat())
     }
+    public static void printList(List<Seat> list) {
+
+        for (Seat seat :
+                list) {
+            System.out.println(" " + seat.getSeatNumber());
+        }
+        System.out.println();
+        System.out.println("===================================================");
+    }
 }
+
