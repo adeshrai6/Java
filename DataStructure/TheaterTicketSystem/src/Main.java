bb62987e3b9df275399c401cf3d450db61f5d3ac
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-05-16 23:48:58 -0400

    Sort list

--- DataStructure/TheaterTicketSystem/src/Main.java
+++ DataStructure/TheaterTicketSystem/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -15,6 +16,17 @@ public class Main {
         }
         System.out.println();
         System.out.println("===================================================");
+    }
+
+    public static void sortList(List<Seat> list) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i+1; j < list.size(); j++) {
+                if (list.get(i).compareTo(list.get(j)) > 0){
+                    Collections.swap(list, i, j);
+                }
+
+            }
+        }
     }
 }
 
